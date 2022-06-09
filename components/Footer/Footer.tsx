9b35359335ad24719204b0ccdac7b9b70be01252
alwaysEnergetic <dhorse443@gmail.com>
2022-06-09 03:52:28 -0400

    updated with new design

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,13 +2,12 @@ export default function Footer() {
   return (
     <footer className="text-3xl text-center absolute bottom-0 w-full p-8 bg-background">
       <p className="text-textColor">
-        Site made by {}
+        Site deployed {}
         <a
           className="text-yellow font-semibold hover:underline "
           target="_blank"
           href="https://github.com/alwaysEnergetic/coding-test-weather-api"
         >
-          John Chang
         </a>{" "}
         @2022 March 8
       </p>
